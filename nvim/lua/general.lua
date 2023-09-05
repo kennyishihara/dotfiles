d6d0d3c03572d34435853bfcf7b524a8b1379e61
kennyishihara <contact@kentaroishihara.com>
2023-09-05 14:49:12 +0800

    Added fold defs

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -23,3 +23,10 @@ vim.opt.shortmess:append("c")
 vim.opt.diffopt:append("iwhite")
 vim.opt.showmatch = true
 vim.opt.wrap = false
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldtext =
+    [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) ]]
+vim.wo.fillchars = "fold: "
+vim.wo.foldnestmax = 3
+vim.wo.foldminlines = 1
