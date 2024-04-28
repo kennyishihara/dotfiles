eb3898b942e6728b4604e3185057ed3d20bd5eca
kennyishihara <contact@kentaroishihara.com>
2024-04-28 13:02:35 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,7 @@
+-- Wezterm config inspired by theopn https://github.com/theopn/dotfiles/tree/main/wezterm
+
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 local config = {
     color_scheme = "Kanagawa (Gogh)",
@@ -6,41 +9,154 @@ local config = {
     font = wezterm.font("Roboto Mono"),
     use_dead_keys = false,
     window_decorations = "RESIZE",
-    hide_tab_bar_if_only_one_tab = true,
     line_height = 1.0,
-    tab_bar_at_bottom = true,
-    keys = {
-      { key = "a", mods = "SUPER|CTRL",  action=wezterm.action{SendString="\x01"}},
-      -- { key = "-", mods = "SUPER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-      -- { key = "\\",mods = "SUPER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-      { key = "s", mods = "SUPER",       action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-      { key = "v", mods = "SUPER",       action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-      { key = "o", mods = "SUPER",       action="TogglePaneZoomState" },
-      -- { key = "z", mods = "SUPER",       action="TogglePaneZoomState" },
-      { key = "c", mods = "SUPER",       action=wezterm.action{CopyTo="Clipboard"}},
-      { key = "h", mods = "SUPER",       action=wezterm.action{ActivatePaneDirection="Left"}},
-      { key = "j", mods = "SUPER",       action=wezterm.action{ActivatePaneDirection="Down"}},
-      { key = "k", mods = "SUPER",       action=wezterm.action{ActivatePaneDirection="Up"}},
-      { key = "l", mods = "SUPER",       action=wezterm.action{ActivatePaneDirection="Right"}},
-      { key = "H", mods = "SUPER|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 5}}},
-      { key = "J", mods = "SUPER|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 5}}},
-      { key = "K", mods = "SUPER|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 5}}},
-      { key = "L", mods = "SUPER|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 5}}},
-      { key = "1", mods = "SUPER",       action=wezterm.action{ActivateTab=0}},
-      { key = "2", mods = "SUPER",       action=wezterm.action{ActivateTab=1}},
-      { key = "3", mods = "SUPER",       action=wezterm.action{ActivateTab=2}},
-      { key = "4", mods = "SUPER",       action=wezterm.action{ActivateTab=3}},
-      { key = "5", mods = "SUPER",       action=wezterm.action{ActivateTab=4}},
-      { key = "6", mods = "SUPER",       action=wezterm.action{ActivateTab=5}},
-      { key = "7", mods = "SUPER",       action=wezterm.action{ActivateTab=6}},
-      { key = "8", mods = "SUPER",       action=wezterm.action{ActivateTab=7}},
-      { key = "9", mods = "SUPER",       action=wezterm.action{ActivateTab=8}},
-      { key = "&", mods = "SUPER|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=true}}},
-      { key = "d", mods = "SUPER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-      -- { key = "x", mods = "SUPER",       action=wezterm.action{CloseCurrentPane={confirm=true}}},
-      { key = "u", mods = "SUPER",       action="ActivateCopyMode" },
-      { key = "i", mods = "SUPER",       action=wezterm.action({ PasteFrom = "PrimarySelection" }) },
+    inactive_pane_hsb = {
+        saturation = 0.24,
+        brightness = 0.5,
     },
+    leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+    keys = {
+        { key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
+        { key = "c",          mods = "LEADER", action = "ActivateCopyMode" },
+
+        -- Panes
+        { key = "s",          mods = "LEADER", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+        { key = "v",          mods = "LEADER", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+        { key = "h",          mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+        { key = "j",          mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+        { key = "k",          mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+        { key = "l",          mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+        { key = "q",          mods = "LEADER", action = act.CloseCurrentPane { confirm = true } },
+        { key = "o",          mods = "LEADER", action = act.TogglePaneZoomState },
+        { key = "r",          mods = "LEADER", action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
+
+        -- Tabs
+        { key = "t",          mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+        { key = "[",          mods = "LEADER", action = act.ActivateTabRelative(-1) },
+        { key = "]",          mods = "LEADER", action = act.ActivateTabRelative(1) },
+        { key = "n",          mods = "LEADER", action = act.ShowTabNavigator },
+        {
+            key = "e",
+            mods = "LEADER",
+            action = act.PromptInputLine {
+                description = wezterm.format {
+                    { Attribute = { Intensity = "Bold" } },
+                    { Foreground = { AnsiColor = "Fuchsia" } },
+                    { Text = "Renaming Tab Title...:" },
+                },
+                action = wezterm.action_callback(function(window, pane, line)
+                    if line then
+                        window:active_tab():set_title(line)
+                    end
+                end)
+            }
+        },
+        -- Key table for moving tabs around
+        { key = "m", mods = "LEADER",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+        -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+        { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+        { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+
+        -- Lastly, workspace
+        { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+    },
+
+}
+
+-- Move around tabs with index
+for i = 1, 9 do
+    table.insert(config.keys, {
+        key = tostring(i),
+        mods = "LEADER",
+        action = act.ActivateTab(i - 1)
+    })
+end
+
+config.key_tables = {
+  resize_pane = {
+    { key = "h",      action = act.AdjustPaneSize { "Left", 1 } },
+    { key = "j",      action = act.AdjustPaneSize { "Down", 1 } },
+    { key = "k",      action = act.AdjustPaneSize { "Up", 1 } },
+    { key = "l",      action = act.AdjustPaneSize { "Right", 1 } },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h",      action = act.MoveTabRelative(-1) },
+    { key = "j",      action = act.MoveTabRelative(-1) },
+    { key = "k",      action = act.MoveTabRelative(1) },
+    { key = "l",      action = act.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
   }
+}
+
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+config.tab_bar_at_bottom = false
+wezterm.on("update-status", function(window, pane)
+    -- Workspace name
+    local stat = window:active_workspace()
+    local stat_color = "#f7768e"
+    -- It's a little silly to have workspace name all the time
+    -- Utilize this to display LDR or current key table name
+    if window:active_key_table() then
+        stat = window:active_key_table()
+        stat_color = "#7dcfff"
+    end
+    if window:leader_is_active() then
+        stat = "LDR"
+        stat_color = "#bb9af7"
+    end
+
+    local basename = function(s)
+        -- Nothing a little regex can't fix
+        return string.gsub(s, "(.*[/\\])(.*)", "%2")
+    end
+
+    -- Current working directory
+    local cwd = pane:get_current_working_dir()
+    if cwd then
+        if type(cwd) == "userdata" then
+            -- Wezterm introduced the URL object in 20240127-113634-bbcac864
+            cwd = basename(cwd.file_path)
+        else
+            -- 20230712-072601-f4abf8fd or earlier version
+            cwd = basename(cwd)
+        end
+    else
+        cwd = ""
+    end
+
+    -- Current command
+    local cmd = pane:get_foreground_process_name()
+    -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+    cmd = cmd and basename(cmd) or ""
+
+    -- Time
+    local time = wezterm.strftime("%H:%M:%S")
+
+    -- Left status (left of the tab line)
+    window:set_left_status(wezterm.format({
+        { Foreground = { Color = stat_color } },
+        { Text = "  " },
+        { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+        { Text = " |" },
+    }))
+
+    -- Right status
+    window:set_right_status(wezterm.format({
+        -- Wezterm has a built-in nerd fonts
+        -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+        { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+        { Text = " | " },
+        { Foreground = { Color = "#e0af68" } },
+        { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+        "ResetAttributes",
+        { Text = " | " },
+        { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+        { Text = "  " },
+    }))
+end)
 
 return config
